34eb6b87788f634f599f7c1581c645a49de31d40
Nils Schaetti <nils.schaetti@unine.ch>
2017-08-22 16:12:53 +0200

    Server again

--- server.py
+++ server.py
@@ -2,19 +2,41 @@
 # -*- coding: utf-8 -*-
 
 # Imports
-import SocketServer
-from server.BlockchainServer import BlockchainServer
+import argparse
+import logging
 
+from net.BlockchainServer import BlockchainServer
 
+# Main function
 if __name__ == "__main__":
-
     # Config
     HOST, PORT = "localhost", 9999
 
+    # Argument parser
+    parser = argparse.ArgumentParser(description="pyBlockchain - Server")
+
+    # Argument
+    parser.add_argument("--host", type=str, help="Hostname address to listen  to", required=True)
+    parser.add_argument("--port", type=int, help="Port number to listen to", default=3818)
+    args = parser.parse_args()
+
+    # Logging
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s-%(name)s-%(levelname)s : %(message)s")
+
     # Instantiate the server, and bind to port 9999
-    blockchain_server = SocketServer.TCPServer((HOST, PORT), BlockchainServer)
+    blockchain_server = BlockchainServer(args.host, args.port)
 
-    # Activate the server
-    blockchain_server.serve_forever()
+    # Starting server
+    logging.info(u"Starting server")
+    blockchain_server.start()
 
+    # Wait the end
+    try:
+        while blockchain_server.isAlive():
+            blockchain_server.join(5)
+        # end while
+    except (KeyboardInterrupt, SystemExit):
+        logging.info(u"Signal received, exiting")
+        blockchain_server.stop()
+    # end try
 # end if
